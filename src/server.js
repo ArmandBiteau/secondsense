0f3e20f690bd7265dd2d386241a7078bd442b65d
Armand <armandbiteau@gmail.com>
2015-11-11 13:31:11 +0100

    update node server

--- src/server.js
+++ src/server.js
@@ -8,24 +8,4 @@ io.sockets.on('connection', function(socket) {
 
     console.log('connection !');
 
-    socket.on('set nickname', function(name) {
-
-        socket.set('nickname', name, function() {
-
-            socket.emit('ready');
-
-        });
-
-    });
-
-    socket.on('msg', function() {
-
-        socket.get('nickname', function(err, name) {
-
-            console.log('Chat message by ', name);
-
-        });
-
-    });
-
 });
